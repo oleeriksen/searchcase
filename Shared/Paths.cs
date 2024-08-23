3f01486e3004129914b91825122eaabdbc143409
oleeriksen <oe@easv.dk>
2024-08-23 09:10:02 +0200

    til .net 8

--- Shared/Paths.cs
+++ Shared/Paths.cs
@@ -3,8 +3,8 @@ namespace Shared
 {
     public class Paths
     {
-        public static string FOLDER = @"/Users/ole/Data/seData/small";
+        public static string FOLDER = @"/Users/ole/Data/seData/medium1";
 
-        public static string DATABASE = @"/Users/oleeriksen/Data/searchDBLarge.db";
+        public static string DATABASE = @"/Users/ole/Data/searchDBmedium1.db";
     }
 }
